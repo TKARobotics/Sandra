26754c51f88cc0c88d1ebdeecac7eead1e757599
Macy Schrepel <macyschrepel@Macys-MacBook.lan>
2021-03-28 17:11:56 -0400

    ghfjghfsjkg

--- src/main.cpp
+++ src/main.cpp
@@ -21,12 +21,12 @@ using namespace vex;
 competition Competition;
 
 // define your global instances of motors and other devices here
+vex::vision Vision1(vex::PORT5);
 
 int leftStickXaxis = Controller1.Axis4.value();
 int rawStrafeInput = leftStickXaxis;
 int rightStickYaxis = (Controller1.Axis2.value() + 126) / 62.5;
 int ArmSpeedPercent = 50;
-vex::vision Vision1(vex::PORT5);
 
 
 /*---------------------------------------------------------------------------*/
@@ -39,11 +39,10 @@ vex::vision Vision1(vex::PORT5);
 /*  not every time that the robot is disabled.                               */
 /*---------------------------------------------------------------------------*/
 
-void pre_auton(void) {
+void pre_auton(void) 
+{
   // Initializing Robot Configuration. DO NOT REMOVE!
-  vexcodeInit();
-
-  
+  vexcodeInit();  
 
   // All activities that occur before the competition starts
   // Example: clearing encoders, setting servo positions, ...
@@ -59,17 +58,24 @@ void pre_auton(void) {
 /*  You must modify the code to add your own robot specific commands here.   */
 /*---------------------------------------------------------------------------*/
 
-void autonomous(void) {
-FrontLeftMotor.rotateFor(vex::directionType::rev, 659.4, vex::rotationUnits::deg);
+void driveForward(int amountForward)
+{
+    FrontRightMotor.startRotateFor(vex::directionType::fwd, amountForward, vex::rotationUnits::deg);
+    FrontLeftMotor.startRotateFor(vex::directionType::fwd, amountForward, vex::rotationUnits::deg);
+    BackRightMotor.startRotateFor(vex::directionType::fwd, amountForward, vex::rotationUnits::deg);
+    BackLeftMotor.rotateFor(vex::directionType::fwd, amountForward, vex::rotationUnits::deg);
+}
+void autonomous(void) 
+{
+  FrontLeftMotor.rotateFor(vex::directionType::rev, 659.4, vex::rotationUnits::deg);
  
-    Brain.Screen.clearLine();
-    Vision1.takeSnapshot(REDBALL);
- 
-    if (Vision1.largestObject.exists) {
-    FrontRightMotor.startRotateFor(vex::directionType::fwd, 1490.24, vex::rotationUnits::deg);
-    FrontLeftMotor.startRotateFor(vex::directionType::fwd, 1490.24, vex::rotationUnits::deg);
-    BackRightMotor.startRotateFor(vex::directionType::fwd, 1490.24, vex::rotationUnits::deg);
-    BackLeftMotor.rotateFor(vex::directionType::fwd, 1490.24, vex::rotationUnits::deg);
+  Brain.Screen.clearLine();
+//  Vision1.takeSnapshot(REDBALL);
+int seesRedBall = Vision1.largestObject.exists;
+
+  if ( seesRedBall )
+  {
+    driveForward(1490);
     
     // bring in code
 
@@ -82,21 +88,15 @@ FrontLeftMotor.rotateFor(vex::directionType::rev, 659.4, vex::rotationUnits::deg
     RightBasketMotor.rotateFor(vex::directionType::rev, 1000, vex::rotationUnits::deg);
 
     FrontRightMotor.rotateFor(vex::directionType::rev, 70.2, vex::rotationUnits::deg);
- 
-    FrontRightMotor.startRotateFor(vex::directionType::fwd, 1689.95, vex::rotationUnits::deg);
-    FrontLeftMotor.startRotateFor(vex::directionType::fwd, 1689.95, vex::rotationUnits::deg);
-    BackRightMotor.startRotateFor(vex::directionType::fwd, 1689.95, vex::rotationUnits::deg);
-    BackLeftMotor.rotateFor(vex::directionType::fwd, 1689.95, vex::rotationUnits::deg);
- 
+
+    driveForward(1690);
+
     //      insert bring in code
- 
+
     FrontRightMotor.rotateFor(vex::directionType::rev, 102.69, vex::rotationUnits::deg);
- 
-    FrontRightMotor.startRotateFor(vex::directionType::fwd, 1158.66, vex::rotationUnits::deg);
-    FrontLeftMotor.startRotateFor(vex::directionType::fwd, 1158.66, vex::rotationUnits::deg);
-    BackRightMotor.startRotateFor(vex::directionType::fwd, 1158.66, vex::rotationUnits::deg);
-    BackLeftMotor.rotateFor(vex::directionType::fwd, 1158.66, vex::rotationUnits::deg);
- 
+
+    driveForward(1159);
+
     LeftBasketMotor.startRotateFor(vex::directionType::fwd, 1000, vex::rotationUnits::deg);
     RightBasketMotor.rotateFor(vex::directionType::fwd, 1000, vex::rotationUnits::deg);
 
@@ -104,25 +104,16 @@ FrontLeftMotor.rotateFor(vex::directionType::rev, 659.4, vex::rotationUnits::deg
 
     LeftBasketMotor.startRotateFor(vex::directionType::rev, 1000, vex::rotationUnits::deg);
     RightBasketMotor.rotateFor(vex::directionType::rev, 1000, vex::rotationUnits::deg);
- 
+
     FrontLeftMotor.rotateFor(vex::directionType::rev, 500, vex::rotationUnits::deg);
- 
-    FrontRightMotor.startRotateFor(vex::directionType::fwd, 1490.24, vex::rotationUnits::deg);
-    FrontLeftMotor.startRotateFor(vex::directionType::fwd, 1490.24, vex::rotationUnits::deg);
-    BackRightMotor.startRotateFor(vex::directionType::fwd, 1490.24, vex::rotationUnits::deg);
-    BackLeftMotor.startRotateFor(vex::directionType::fwd, 1490.24, vex::rotationUnits::deg);
+
+    driveForward(1490);
     
+  }
+  else
+  {
 
-
-    //      }
- 
-    //  
- 
-   //   else  {
-    //     VisionSensorPath.takeSnapshot(REDBALL);
-    //   }
-    //}
-
+  }
   //while (Vision1.value() != Vision1.takeSnapshot(REDBALL);
 }
 
@@ -136,8 +127,8 @@ FrontLeftMotor.rotateFor(vex::directionType::rev, 659.4, vex::rotationUnits::deg
 /*  You must modify the code to add your own robot specific commands here.   */
 /*---------------------------------------------------------------------------*/
 
-void usercontrol(void) { 
-
+void usercontrol(void) 
+{ 
   while (1) 
   {
     // This is the main execution loop for the user control program.
@@ -163,11 +154,9 @@ void usercontrol(void) {
      {
         LeftBasketMotor.stop( vex::brakeType::brake );
         RightBasketMotor.stop( vex::brakeType::brake );
-     }
-      
-
-      }
-
+     }     
+  }
+}
 
 
 
@@ -177,7 +166,7 @@ void usercontrol(void) {
     // update your motors, etc.
     // ........................................................................
 
-    
+
 
 
 //
@@ -197,6 +186,4 @@ int main()
   {
     wait( 100, msec );
   }
-}
-}
 }
